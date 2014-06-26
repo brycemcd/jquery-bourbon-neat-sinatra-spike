07f003b5557eea7b15446e5379ac2a056ba2b3a8
Bryce McDonnell <bryce@cricketscircle.com>
2014-06-26 14:45:28 -0400

    adds a js modal window... that works!

--- spike.rb
+++ spike.rb
@@ -1,3 +1,13 @@
 get '/' do
   erb :index
 end
+
+get '/some-crap/:i.html' do
+  str = 'This is some crap for ' << params[:i]
+  <<-STR
+          <div id="test-popup-<%= i %>" class="white-popup mfp-hide">
+            Hello World, this is popup <%= i %>
+            #{str}
+          </div>
+  STR
+end
